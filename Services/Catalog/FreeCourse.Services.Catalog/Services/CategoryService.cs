3a8f30917687f338b5a8703d252b0525db91350a
coderdeniz <denizdumanresmi@gmail.com>
2024-01-24 14:18:21 +0300

    4 / bölüm-2

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CategoryService.cs
@@ -32,8 +32,10 @@ namespace FreeCourse.Services.Catalog.Services
                 StatusCodes.Status200OK);
         }
 
-        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        public async Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto)
         {
+            var category = _mapper.Map<Category>(categoryDto);
+
             await _categoryCollection.InsertOneAsync(category);
 
             return Response<CategoryDto>.Success(
