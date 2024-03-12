af5ced5a4639b3ae0f45994c6ed9c268b0cee8e2
coderdeniz <denizdumanresmi@gmail.com>
2024-03-12 23:54:56 +0300

    bölüm - 9 / 1. kısım

---
+++ Gateways/FreeCourse.Gateway/Program.cs
@@ -0,0 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+
+var app = builder.Build();
+
+await app.UseOcelot();
+
+app.Run();
